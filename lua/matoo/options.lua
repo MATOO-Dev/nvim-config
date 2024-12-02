b2d58d07f41e29fc6646f29863bab6df3d5f07d6
MATOO-Dev <matoo-dev@proton.me>
2024-12-02 02:05:08 +0100

    Added more options

--- lua/matoo/options.lua
+++ lua/matoo/options.lua
@@ -29,3 +29,32 @@ vim.opt.showmode = false
 
 -- thin cursor in insert, block otherwise, and make it blink
 vim.opt.guicursor = "n-v-r:block,i:ver25,a:blinkwait700-blinkoff400-blinkon250"
+
+-- disable automatic backups, instead give undotree more access
+vim.opt.swapfile = false
+vim.opt.backup = false
+-- this needs to be adjusted for NixOS
+-- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- disable search highlighting (subject to change) and enable incremental search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- enable colors in the TUI and pull them from terminal if possible
+vim.opt.termguicolors = true
+
+-- minimum cursor distance to top/bottom when scrolling (unless BOF/EOF)
+vim.opt.scrolloff = 15
+
+-- always show sign column
+vim.opt.signcolumn = "yes"
+
+-- show bar at n chars to indicate long lines
+vim.opt.colorcolumn = "80"
+
+-- enable @ in file names
+vim.opt.isfname:append("@-@")
+
+-- fast updatetime
+vim.opt.updatetime = 50
