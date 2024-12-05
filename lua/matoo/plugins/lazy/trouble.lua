876dc959a49d988831988c436634b463d1e285f9
MATOO-Dev <matoo-dev@proton.me>
2024-12-05 17:16:51 +0100

    Updated trouble keybinds

--- lua/matoo/plugins/lazy/trouble.lua
+++ lua/matoo/plugins/lazy/trouble.lua
@@ -8,17 +8,8 @@ return {
 		require("trouble").setup {
 			warn_no_results = false,
 			open_no_results = true,
+			focus = true,
 		}
 		vim.keymap.set("n", "<leader>tr", "<cmd>Trouble diagnostics toggle<cr>")
-		-- todo: this is a bit nicer, get this working
-		-- vim.keymap.set("n", "<leader>tr",
-		-- 	function()
-		-- 		if vim.cmd("Trouble diagnostics is_open") then
-		-- 			vim.cmd("Trouble close")
-		-- 		else
-		-- 			vim.cmd("Trouble diagnostics focus")
-		-- 		end
-		-- 	end
-		-- )
 	end,
 }
