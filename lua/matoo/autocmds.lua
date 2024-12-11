4932c5c78f239973aa2a58d19b46968d5c9e27a7
MATOO-Dev <matoo-dev@proton.me>
2024-12-11 17:52:05 +0100

    Enabled wrapping for typst and txt

--- lua/matoo/autocmds.lua
+++ lua/matoo/autocmds.lua
@@ -1,10 +1,15 @@
-local ftgroup = vim.api.nvim_create_augroup("ft", {clear=true})
+local ftgroup = vim.api.nvim_create_augroup("ft", { clear = true })
 
-vim.api.nvim_create_autocmd(
-	"BufReadPost",
-	{
-		pattern = "*.typ",
-		command = "TypstPreview",
-		group = ftgroup,
-	}
-)
+-- enable wrapping for certain file types
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = { "*.typ", "*.txt" },
+	command = "setlocal wrap",
+	group = ftgroup,
+})
+
+-- automatically open typst preview
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = "*.typ",
+	command = "TypstPreview",
+	group = ftgroup,
+})
